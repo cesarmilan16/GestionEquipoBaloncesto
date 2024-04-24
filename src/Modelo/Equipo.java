42eb9ea88a469a1cec4d37168492beff7f249e36
cesarmilan16 <cesar3bfermi@gmail.com>
2024-04-24 11:30:56 +0200

    añadimos funcion alta del equipo

--- src/Modelo/Equipo.java
+++ src/Modelo/Equipo.java
@@ -7,4 +7,13 @@ public class Equipo {
     private String ciudad;
     private Entrenador entrenador;
     private ArrayList<Jugador> plantilla;
+
+    public Equipo(String nombre, String ciudad){
+        this.nombre = nombre;
+        this.ciudad = ciudad;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
 }
--- src/Modelo/Liga.java
+++ src/Modelo/Liga.java
@@ -1,5 +1,37 @@
 package Modelo;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Liga {
+    private ArrayList<Equipo> equipos = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
     
+    public void altaEquipo(){
+        System.out.println("***************************");
+        System.out.println("******* Alta Equipo *******");
+        System.out.println("***************************");
+        System.out.println("Nombre equipo: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Ciudad equipo");
+        String ciudad = scanner.nextLine();
+
+        Equipo equipoEncontrado = null;
+        int contador = 0;
+        while (equipoEncontrado != null && contador < equipos.size()) {
+            if (equipos.get(contador).getNombre().equals(nombre)) {
+                equipoEncontrado = equipos.get(contador);
+            }
+            contador++;
+        }
+
+        if (equipoEncontrado == null) {
+            Equipo equipo = new Equipo(nombre, ciudad);
+            equipos.add(equipo);            
+        }
+        else{
+            System.out.println("Ese equipo ya existe.");
+        }
+    }
 }
